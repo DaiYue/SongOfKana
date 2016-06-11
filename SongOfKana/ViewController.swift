0fe695f3f11d1280a7a4b0f95875609da2f39397
赵嵩阳 <zhaosongy@126.com>
2016-06-11 22:41:18 +0800

    Initial Commit

---
+++ SongOfKana/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SongOfKana
+//
+//  Created by 赵嵩阳 on 16/6/11.
+//  Copyright © 2016年 FindTheLamp Studio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
